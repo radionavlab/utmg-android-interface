6a6f31bf88d7c1af1a99d51de3762e3bc22137d6
Siddarth Kaki <siddarthkaki@utexas.edu>
2017-03-13 12:09:04 -0500

    Added ROS integration to ROSCam activity     - able to connect via ROS, but camera interface force quits upon launch

--- app/src/main/java/utmg/android_interface/ROSCam.java
+++ app/src/main/java/utmg/android_interface/ROSCam.java
@@ -1,13 +1,86 @@
 package utmg.android_interface;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+import org.ros.address.InetAddressFactory;
+import org.ros.android.RosActivity;
+import org.ros.android.view.camera.RosCameraPreviewView;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+import android.util.Log;
+import java.io.IOException;
 
-public class ROSCam extends AppCompatActivity {
+public class ROSCam extends RosActivity {
+
+    private int cameraId;
+    private RosCameraPreviewView rosCameraPreviewView;
+
+    public ROSCam() {
+        super("ROSCam", "ROSCam");//, URI.create("http://192.168.1.10:11311"));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_roscam);
+
+        rosCameraPreviewView = (RosCameraPreviewView) findViewById(R.id.ros_camera_preview_view);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            int numberOfCameras = Camera.getNumberOfCameras();
+            final Toast toast;
+            if (numberOfCameras > 1) {
+                cameraId = (cameraId + 1) % numberOfCameras;
+                rosCameraPreviewView.releaseCamera();
+                rosCameraPreviewView.setCamera(Camera.open(cameraId));
+                toast = Toast.makeText(this, "Switching cameras.", Toast.LENGTH_SHORT);
+            } else {
+                toast = Toast.makeText(this, "No alternative cameras to switch to.", Toast.LENGTH_SHORT);
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    toast.show();
+                }
+            });
+        }
+        return true;
+    }
+
+
+    @Override
+    protected void init(NodeMainExecutor nodeMainExecutor)
+    {
+        cameraId = 0;
+
+        rosCameraPreviewView.setCamera(Camera.open(cameraId));
+        try
+        {
+            java.net.Socket socket = new java.net.Socket(getMasterUri().getHost(), getMasterUri().getPort());
+            java.net.InetAddress local_network_address = socket.getLocalAddress();
+            socket.close();
+            NodeConfiguration nodeConfiguration =
+                    NodeConfiguration.newPublic(local_network_address.getHostAddress(), getMasterUri());
+            nodeMainExecutor.execute(rosCameraPreviewView, nodeConfiguration);
+        }
+
+        catch (IOException e)
+        {
+            // Socket problem
+            Log.e("Camera Tutorial", "socket error trying to get networking information from the master uri");
+        }
+
     }
 }
