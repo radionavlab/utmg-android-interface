c77c3d410e9986fcbda774f3b544b4b3773e3a33
tuckerhaydon <tuckerhaydon@gmail.com>
2017-10-14 17:19:24 -0500

    Updated gitignore and remove .idea and local.properties files

--- app/src/main/java/utmg/android_interface/QuadUtils/Point3.java
+++ app/src/main/java/utmg/android_interface/QuadUtils/Point3.java
@@ -3,48 +3,81 @@ package utmg.android_interface.QuadUtils;
 import org.ros.message.Time;
 
 /**
- * Created by jtb20 on 7/20/2017.
+ * Created by jtb20 on 7/20/2017. Modified by Tucker Haydon on 10/14/2017.
  */
 
+/**
+ * Immutable data structure containing a 3-tuple coordinate and a time of creation.
+ */
 public class Point3 {
 
-    private float x,y,z;
-    private Time t;
+    /* Coordinate in 3-space */
+    public final float x,y,z;
 
-    public Point3(float x, float y, float z, Time t) {
+    /* Time of creation */
+    public final Time t;
+
+    /**
+     * Constructor
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param z z coordinate
+     * @param t time of creation
+     */
+    public Point3(
+            final float x,
+            final float y,
+            final float z,
+            final Time t) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.t = t;
     }
-    public Point3(float x, float y, float z) {
+
+    /**
+     * Constructor. Constructs ROS time from current system time.
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param z z coordinate
+     */
+    public Point3(
+            final float x,
+            final float y,
+            final float z) {
         this.x = x;
         this.y = y;
         this.z = z;
         t = new Time(Time.fromMillis(System.currentTimeMillis()));
     }
-    public float getX() { return x; }
-    public float getY() { return y; }
-    public float getZ() { return z; }
-    public Time getTime() {return t;}
-    public void setX(float x) { this.x = x; }
-    public void setY(float y) { this.y = y; }
-    public void setZ(float z) { this.z = z; }
 
-    public void setPoint(float x, float y, float z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    /**
+     * Constructs and returns a point that is offset from the current point spacially but at the same moment in time.
+     * @param dx
+     * @param dy
+     * @param dz
+     * @return A point that is offset from the current point spacially but at the same moment in time.
+     */
+    public Point3 offset(
+            final float dx,
+            final float dy,
+            final float dz,
+            final float dt) {
+
+        return new Point3(
+                this.x + dx,
+                this.y + dy,
+                this.z + dz,
+                this.t);
     }
 
-    public void offset(float dx, float dy, float dz) {
-        x += dx;
-        y += dy;
-        z += dz;
+    /**
+     * Calculates the spacial distance between this point and another point.
+     * @param other
+     * @return
+     */
+    public float distanceTo(
+            final Point3 other) {
+        return (float) (Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2) + Math.pow(other.z - this.z, 2)));
     }
-
-    public float distanceTo(Point3 other) {
-        return (float) Math.sqrt(Math.pow(other.getX()-x,2) + Math.pow(other.getY()-y,2) + Math.pow(other.getZ()-z,2));
-    }
-    public void setTime(Time time) { this.t = time; }
 }
